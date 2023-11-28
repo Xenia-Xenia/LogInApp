d7219117d80b0538dd2f329a7b923bebadafc2bd
Xenia Zagareva <xeniazagareva@gmail.com>
2023-11-28 12:28:14 +0300

    V 0.1

---
+++ LoginApp/LoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoginViewController.swift
+//  LoginApp
+//
+//  Created by Ксения Загарьева on 27.11.2023.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+    
+    @IBOutlet var login: UITextField!
+    @IBOutlet var password: UITextField!
+    
+    @IBOutlet var logInButton: UIButton!
+    @IBOutlet var forgotUserNameButton: UIButton!
+    @IBOutlet var forgotPasswordButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func logInButtonTapped() {
+    }
+    
+    @IBAction func forgotUserNameButtonTapped() {
+    }
+    
+    @IBAction func forgotPasswordButtonTapped() {
+    }
+}
+
---
+++ LoginApp/WelcomeMessageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeMessageViewController.swift
+//  LoginApp
+//
+//  Created by Ксения Загарьева on 28.11.2023.
+//
+
+import UIKit
+
+final class WelcomeMessageViewController: UIViewController {
+    @IBOutlet var welcomeMessage: UILabel!
+    @IBOutlet var logOutButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
